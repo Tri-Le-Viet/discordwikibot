961de7bb5352d61901a162dbcaba549ff0c35717
tle-viet <tri.leviet@protonmail.com>
2021-01-05 15:32:12 +1000

    Ver 1.0.2

--- bot.js
+++ bot.js
@@ -4,7 +4,16 @@ const client = new Discord.Client();
 const fs = require("fs");
 var history = {};
 const languages = fs.readFileSync("./language_codes.txt", "utf-8").split("\n");
-var role;
+
+const help = "```Wikibot ver 1.0.2\nTo change the command character, message the bot '@Wikibot config new_char'.\n\
+By default it is set to '!'\n\nCommands:\n\
+  !wiki query        searches Wikipedia for the article that is the closest match for 'query' (defaults to English)\n\
+  !wiki-lang query   searches Wikipedia for the article in the specified language for the closest match to 'query'\n\
+  !wikiNext          selects the next closest match for the last searched 'query'\n\
+  !random            selects a random English Wikipedia article\n\
+  !random lang       selects a random Wikipedia in the specified language```";
+
+var command = "!";
 
 function searchWiki(msg, query, number, lang) {
   console.log("Wiki request: " + query);
@@ -25,7 +34,7 @@ function searchWiki(msg, query, number, lang) {
       .then(function(response){console.log(response);
         if(response[1].length === number) {
           msg.reply("Top result: " + response[1][number-1] + "\n" + response[3][number-1] +
-          "\nNot what you're looking for? Try the the next result with command '!wikiNext'")
+          "\nNot what you're looking for? Try the the next result with command '" + command + "wikiNext'");
         } else {
           if (number == 1) {
             msg.reply("No results found for '" + query + "'. Try refining your search terms.");
@@ -48,21 +57,24 @@ client.on("ready", () => {
 });
 
 client.on("message", msg => {
-  if (msg.mentions.has(client.user) && msg.content.toLowerCase().includes("help")) {
-    msg.reply("haha get fuked nerd");
+  if (msg.mentions.has(client.user) && msg.content.toLowerCase().includes("config")) {
+    command = msg.content.split("config")[1][1];
 
-  } else if (msg.content === "!wikiNext") {
+  } else if (msg.content === command + "help") {
+    msg.reply(help);
+
+  } else if (msg.content === command + "wikiNext") {
     var number = history[msg.author["id"]][1] + 1;
     history[msg.author["id"]][1] = number;
     searchWiki(msg, history[msg.author["id"]][0], number, history[msg.author["id"]][2]);
     var query;
 
-  } else if (msg.content.startsWith("!wiki")) {
+  } else if (msg.content.startsWith(command + "wiki")) {
     if (msg.content[5] == "-") {
       var lang = msg.content.split(" ")[0].split("-")[1];
       if (!languages.includes(lang)) {
         msg.reply("Not a valid wikipedia language.\n\
-See 'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code' for a complete list of languages.");
+See 'https://en.wikipedia.org/wiki/List_of_Wikipedias' for a complete list of supported languages.");
 
         history[msg.author["id"]] = [];
         return;
@@ -78,8 +90,8 @@ See 'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_
     }
     searchWiki(msg, query, 1, lang);
 
-  } else if (msg.content.startsWith("!random")) {
-    if (msg.content === "!random") {
+  } else if (msg.content.startsWith(command + "random")) {
+    if (msg.content === command + "random") {
       lang = "en";
     } else {
       lang = msg.content.slice(8);
@@ -88,10 +100,9 @@ See 'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_
     if (!languages.includes(lang)) {
       msg.reply("Not a valid wikipedia language.\n\
 See 'https://meta.wikimedia.org/wiki/Template:List_of_language_names_ordered_by_code' for a complete list of languages.");
-        return;
+    } else {
+      randomArticle(msg, lang);
     }
-
-    randomArticle(msg, lang);
   }
 });
 
